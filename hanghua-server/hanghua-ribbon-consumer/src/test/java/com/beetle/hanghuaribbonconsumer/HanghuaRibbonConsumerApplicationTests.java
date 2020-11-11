772d8e006c72c4de06736c5c32e30a54927de2f1
zhaojie777 <zhaojie777j@gmail.com>
2020-11-11 23:40:38 +0800

    2020-11-11

---
+++ hanghua-server/hanghua-ribbon-consumer/src/test/java/com/beetle/hanghuaribbonconsumer/HanghuaRibbonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beetle.hanghuaribbonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghuaRibbonConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ hanghua-server/hanghua-zuul/src/test/java/com/beetle/hanghuazuul/HanghuaZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beetle.hanghuazuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghuaZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
