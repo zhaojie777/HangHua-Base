3bdc5c5519d7aa4b7c9327cd78142808a9e4fdd0
zhaojie <zhaojie777j@gmail.com>
2022-06-08 17:16:08 +0800

    delete oauth-resource-server and add gateway

---
+++ hanghua-server/hanghua-gateway/src/main/java/com/beetle/hanghua/gateway/HanghuaGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.beetle.hanghua.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther zhaojie
+ * @date 2022/06/08 17:09
+ **/
+@SpringBootApplication
+public class HanghuaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghuaGatewayApplication.class, args);
+    }
+}
