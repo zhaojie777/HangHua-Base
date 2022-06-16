1d673ff21691c2f498e8aa5079bcd76a5c4119d3
zhaojie <zhaojie777j@gmail.com>
2022-06-16 13:56:12 +0800

    add 通信模块

---
+++ hanghua-server/hanghua-im/src/test/java/com/beetle/hanghua/im/HanghuaImApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beetle.hanghua.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghuaImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
