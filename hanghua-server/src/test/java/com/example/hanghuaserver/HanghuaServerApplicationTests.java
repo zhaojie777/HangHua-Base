0fb63f787a1373f6cee512950172044bfec5a2c1
zhaojie <zhaojie777j@gmail.com>
2023-09-19 17:16:35 +0800

    add single

---
+++ hanghua-server/src/test/java/com/example/hanghuaserver/HanghuaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghuaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghuaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
